76276a5c98dade0f4068d153816ac574d54f270d
vannyratanak <vannyratanakchheang@gmail.com>
2023-12-07 19:08:08 +0100

    monitoring

--- release/monitoring.sh
+++ release/monitoring.sh
@@ -1,7 +1,9 @@
-kubectl apply -f ./monitoring/grafana/grafana-datasource-config.yaml
-kubectl apply -f ./monitoring/grafana/deployment.yaml
-kubectl apply -f ./monitoring/grafana/service.yaml
+# kubectl apply -f ./monitoring/grafana/grafana-datasource-config.yaml
+# kubectl apply -f ./monitoring/grafana/deployment.yaml
+# kubectl apply -f ./monitoring/grafana/service.yaml
 
-kubectl apply -f ./monitoring/prometheus/clusterRole.yaml
-kubectl apply -f ./monitoring/prometheus/config-map.yaml
-kubectl apply -f ./monitoring/prometheus/prometheus-deployment.yaml
+# kubectl apply -f ./monitoring/prometheus/clusterRole.yaml
+# kubectl apply -f ./monitoring/prometheus/config-map.yaml
+# kubectl apply -f ./monitoring/prometheus/prometheus-deployment.yaml
+
+kubectl apply -f ./monitoring/grafana/grafana.yaml
