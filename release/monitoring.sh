e82e5d61ba6c76d6ba02fb48e5958630ca5f4565
vannyratanak <vannyratanakchheang@gmail.com>
2023-12-07 18:17:13 +0100

    monitoring

--- release/monitoring.sh
+++ release/monitoring.sh
@@ -2,7 +2,7 @@ kubectl create namespace monitoring
 
 kubectl apply -f ./monitoring/grafana/grafana-datasource-config.yaml
 kubectl apply -f ./monitoring/grafana/deployment.yaml
-kubectl apply -f ./monitoring/grafana/service.yam
+kubectl apply -f ./monitoring/grafana/service.yaml
 
 kubectl apply -f ./monitoring/prometheus/clusterRole.yaml
 kubectl apply -f ./monitoring/prometheus/config-map.yaml
