31cfd5a918f09ac4c3efd025ba65b72c43e34165
vannyratanak <vannyratanakchheang@gmail.com>
2023-12-07 19:41:23 +0100

    monitoring

--- release/monitoring.sh
+++ release/monitoring.sh
@@ -2,8 +2,9 @@
 # kubectl apply -f ./monitoring/grafana/deployment.yaml
 # kubectl apply -f ./monitoring/grafana/service.yaml
 
-kubectl apply -f ./monitoring/prometheus/clusterRole.yaml
-kubectl apply -f ./monitoring/prometheus/config-map.yaml
-kubectl apply -f ./monitoring/prometheus/prometheus-deployment.yaml
+# kubectl apply -f ./monitoring/prometheus/clusterRole.yaml
+# kubectl apply -f ./monitoring/prometheus/config-map.yaml
+# kubectl apply -f ./monitoring/prometheus/prometheus-deployment.yaml
 
+kubectl apply -f ./monitoring/prometheus.yaml
 kubectl apply -f ./monitoring/grafana.yaml
